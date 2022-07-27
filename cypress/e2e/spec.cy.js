6b3683b6a8efdff90913b0cc565866aecf5e26d0
Sam Brown <sam.brown@madetech.com>
2022-07-27 16:06:04 +0100

    initial commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('landing page', () => {
+  it('should go to the landing page', () => {
+    cy.visit('localhost:3000')
+  })
+  it('finds the content "name of service"', () => {
+    cy.visit('localhost:3000')
+
+    cy.contains('Name of service')
+  })
+})
+
