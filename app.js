c76e51f9320f73561327ac9afbd67d594687c767
Sam Brown <sam.brown@madetech.com>
2022-08-04 13:26:43 +0100

    use ejs files to render database info to frontend

--- app.js
+++ app.js
@@ -4,20 +4,29 @@ const app = express()
 const port = 3000
 const path = require("path")
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname , "/pages/index.html"))
 })
 
 app.get('/summary', (req, res) => {
-  res.sendFile(path.join(__dirname , "/pages/summary.html"))
+  var cmd = 'SELECT * from user_details WHERE id = 1';
+  db.query(cmd, function(err, result, fields) {
+    if (err) throw err;
+    res.render('summary',{result: result.rows[0].name});
+  })
+  //res.sendFile(path.join(__dirname , "/pages/summary.html"))
 })
 
-app.get('/users', db.getUsers)
-app.get('/ibrahima', db.getUserById)
+// app.get('/users', db.getUsers)
+// app.get('/ibrahima', db.getUserById)
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.set('views', path.join(__dirname, '/pages'));
 
 app.use('/scripts', express.static(path.join(__dirname, '/scripts')))
 
--- scripts/queries.js
+++ scripts/queries.js
@@ -14,31 +14,10 @@ const pool = new Pool({
 
 pool.connect()
 
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM user_details ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const getUserById = (request, response) => {
-  const id = 1 //sessionStorage.getItem('id')
-
-  pool.query("SELECT * FROM user_details WHERE id = $1",[id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
 function namefinder(){
   document.getElementById("namesummary").innerHTML = getUserById();
 }
 
 module.exports = {
-	getUsers,
-  getUserById
+	query: (text, params) => pool.query(text, params),
 }
