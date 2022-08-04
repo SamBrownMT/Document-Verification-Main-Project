2a80089a022e8c32779dccc17542ab398e3c65ff
dave-t-MT <david.trainor@madetech.com>
2022-08-04 12:35:05 +0100

    summary rendering on pug, no dynamic content yet

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express()
 const port = 3000
 const path = require("path")
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname , "/pages/index.html"))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname , "/pages/index.html"))
+// })
 
-app.get('/summary', (req, res) => {
-  res.sendFile(path.join(__dirname , "/pages/summary.html"))
-})
+// app.get('/summary', (req, res) => {
+//   res.sendFile(path.join(__dirname , "/pages/summary.html"))
+// })
 
 app.get('/users', db.getUsers)
 app.get('/ibrahima', db.getUserById)
@@ -22,3 +22,17 @@ app.listen(process.env.PORT || port, () => {
 app.use('/scripts', express.static(path.join(__dirname, '/scripts')))
 
 app.use('/styles', express.static(path.join(__dirname, '/styles')))
+
+// started using template engines
+
+app.set('views', './views')
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/summary', (req, res) => {
+  res.render('summary', { })
+})
