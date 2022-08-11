96e91bd1fa41c6ea9f92a00062e5faebbee882d5
Barbora Kaminskas <106600778+babskami@users.noreply.github.com>
2022-08-11 16:05:13 +0100

    login page update

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname , "/pages/index.html"))
 })
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname , "/pages/login.html"))
+})
+
 app.use('/summary', summary)
 
 app.listen(process.env.PORT || port, () => {
